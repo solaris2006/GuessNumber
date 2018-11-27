430f04c6547deb63687dc9d4b8cf60362c4dd8cd
solaris2006 <cristian.stefan@gmail.com>
2018-11-27 18:58:31 +0200

    Add project files.

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            string userInput;
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 101);
+            bool guessed = false;
+            Console.WriteLine("Please enter a number between 1 and 100");
+            
+
+            while (!guessed)          
+            {
+                userInput = Console.ReadLine();
+                if (Int32.TryParse(userInput, out int result))
+                {
+
+                    if (result > 1 && result < 100)
+                    {
+
+                        if (result == randomNumber)
+                        {
+                            Console.WriteLine("You have guessed the number which is {0}", userInput);
+                            guessed = true;
+                        }
+                        else if (result < randomNumber)
+                        {
+                            Console.WriteLine("The number is smaller than the target numer , try again");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The number is  higher than the target number");
+                        }
+
+                    } else
+                    {
+                        Console.WriteLine("Please enter a number between 1 and 100");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
